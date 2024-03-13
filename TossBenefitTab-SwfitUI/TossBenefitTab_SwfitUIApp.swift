151197fafa8ae31c5c1ef5dc7c4ccea304b03810
wodnd <wodnd0418@naver.com>
2024-03-13 15:07:41 +0900

    Initial Commit

---
+++ TossBenefitTab-SwfitUI/TossBenefitTab_SwfitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TossBenefitTab_SwfitUIApp.swift
+//  TossBenefitTab-SwfitUI
+//
+//  Created by Wodnd on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct TossBenefitTab_SwfitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
