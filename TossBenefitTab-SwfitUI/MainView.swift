742ee543a358f8f1aba386d7dd335dbc8b413521
wodnd <wodnd0418@naver.com>
2024-03-20 16:20:18 +0900

    design: main

--- TossBenefitTab-SwfitUI/MainView.swift
+++ TossBenefitTab-SwfitUI/MainView.swift
@@ -9,16 +9,64 @@ import SwiftUI
 
 struct MainView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("SwiftUI!")
+        NavigationView{
+            List(){
+                HStack(spacing: 20){
+                    Image("ic_point")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 80)
+                    VStack(alignment: .leading){
+                        Text("내 포인트")
+                            .foregroundColor(.gray)
+                        Text("0원")
+                    }
+                    .font(.system(size: 16, weight: .bold))
+                }
+                .listRowSeparator(.hidden)
+                
+                VStack(spacing: 20){
+                    Image(systemName: "sparkles")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 80)
+                    Text("10원 받기")
+                        .font(.system(size: 20, weight: .bold))
+                    Button(action: {
+                        print("clicked")
+                    }, label: {
+                        Text("누르고 10원 받기")
+                            .font(.system(size: 16, weight: .bold))
+                    })
+                    .frame(width:160, height: 50)
+                    .background(.blue)
+                    .cornerRadius(10)
+                }
+                .padding(EdgeInsets(top: 30, leading: 100, bottom: 30, trailing: 100))
+                .background(.gray.opacity(0.3))
+                .cornerRadius(10)
+                .listRowSeparator(.hidden)
+                
+                HStack(spacing: 20){
+                    Image("ic_point")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 80, height: 80)
+                    VStack(alignment: .leading){
+                        Text("내 포인트")
+                        Text("0원")
+                    }
+                }
+                .listRowSeparator(.hidden)
+            }
+            .listStyle(.plain)
+            .navigationTitle("혜택")
         }
-        .padding()
     }
 }
 
 #Preview {
     MainView()
+        .preferredColorScheme(.dark)
 }
